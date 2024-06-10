ebf0a1ba523e672b9c9758a91d94a31826256d81
Igel <mathias.weller@cnrs.fr>
2024-06-10 14:13:34 +0200

    style guides

--- cpp/hsel.cpp
+++ cpp/hsel.cpp
@@ -24,20 +24,24 @@
 std::unordered_map<uint32_t, uint32_t> hsel_options;
 
 extern "C" void random_hsel(uint32_t n, uint32_t k, uint32_t* result) {
-  hsel_options.clear();
-  for (uint32_t i = 0; i != k; ++i) result[i] = i;
-  for (uint32_t i = 0; i != k; ++i) {
-    const uint32_t r = randbelow(n--) + i;
+    hsel_options.clear();
+    for (uint32_t i = 0; i != k; ++i) result[i] = i;
+    for (uint32_t i = 0; i != k; ++i) {
+        const uint32_t r = randbelow(n--) + i;
 
-    if(r >= k) {
-      const auto res_i = result[i];
-      const auto [fr, success] = hsel_options.emplace(r, res_i);
-      if(!success) {
-        result[i] = fr->second;
-        fr->second = res_i;
-      } else result[i] = r;
-    } else std::swap(result[i], result[r]);
-  }
+        if (r < k) {
+            std::swap(result[i], result[r]);
+        } else {
+            const auto res_i = result[i];
+            const auto [fr, success] = hsel_options.emplace(r, res_i);
+            if (success) {
+                result[i] = r;
+            } else {
+                result[i] = fr->second;
+                fr->second = res_i;
+            }
+        }
+    }
 }
 
 extern "C" void sorted_hsel(uint32_t n, uint32_t k, uint32_t* result) {
